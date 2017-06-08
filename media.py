2bb50095892f2fb97df2af0d7cc43de308238415
Rishabh Agarwal Jain <rishab.mps@gmail.com>
2017-06-08 10:49:53 +0530

    Made code PEP 8 compatible

--- media.py
+++ media.py
@@ -5,7 +5,8 @@ import webbrowser
 
 class Movie():
     # Constructor
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
